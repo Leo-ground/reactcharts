a39f1bbdc695dd77e212b54d842f7b1abb2e4376
leo_ground <piscesljy@gmail.com>
2021-03-13 19:56:50 +0900

    Update nomal

--- src/App.js
+++ src/App.js
@@ -16,64 +16,58 @@ class App extends Component{
     this.getChartData();
     
   }
-
-  handleState=(update)=>{
+  
+  getChartData(){
+    //Ajax calls here
     axios({
       method: "get",
       url: "http://localhost/chart/month/sale",
       responseType: "json"
     }).then(function (response) {
-        console.log(response);
-        this.setState({
-          chartData:{
-            
-            labels:['1', '2', '3', '4', '5', '6','7','8','9','10','11','12'],
-            datasets:[
-                {
-                    label:'Population',
-                    data:[
-                        117594,
-                        131045,
-                        153060,
-                        106519,
-                        105162,
-                        95072,
-                        153060,
-                        106519,
-                        105162,
-                        106519,
-                        105162,
-                        95072
-                    ],
-                    backgroundColor:[
-                        'rgba(255,99,132,0.6)',
-                        'rgba(54,162,235,0.6)',
-                        'rgba(255,206,86,0.6)',
-                        'rgba(75,192,192,0.6)',
-                        'rgba(153,102,255,0.6)',
-                        'rgba(255,159,64,0.6)',
-                        'rgba(255,99,132,0.6)',
-                        'rgba(255,206,86,0.6)',
-                        'rgba(75,192,192,0.6)',
-                        'rgba(153,102,255,0.6)',
-                        'rgba(255,159,64,0.6)',
-                        'rgba(54,162,235,0.6)'
-                    ]
-                }
-            ]               
-    
-    
-          },
-        }); //end setstate
+        console.log(response);       
 
     }); //end then
-   
-  }
-
-  getChartData(){
-    //Ajax calls here
     
+    this.setState({
+      chartData:{            
+        labels:['1', '2', '3', '4', '5', '6','7','8','9','10','11','12'],
+        datasets:[
+            {
+                label:'Population',
+                data:[
+                    117594,
+                    131045,
+                    153060,
+                    106519,
+                    105162,
+                    95072,
+                    153060,
+                    106519,
+                    105162,
+                    106519,
+                    105162,
+                    95072
+                ],
+                backgroundColor:[
+                    'rgba(255,99,132,0.6)',
+                    'rgba(54,162,235,0.6)',
+                    'rgba(255,206,86,0.6)',
+                    'rgba(75,192,192,0.6)',
+                    'rgba(153,102,255,0.6)',
+                    'rgba(255,159,64,0.6)',
+                    'rgba(255,99,132,0.6)',
+                    'rgba(255,206,86,0.6)',
+                    'rgba(75,192,192,0.6)',
+                    'rgba(153,102,255,0.6)',
+                    'rgba(255,159,64,0.6)',
+                    'rgba(54,162,235,0.6)'
+                ]
+            }
+        ]               
 
+
+      },
+    }); //end setstate
     
   }
 
